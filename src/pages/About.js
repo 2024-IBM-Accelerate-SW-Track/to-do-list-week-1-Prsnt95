4c7909eaef03ba157ed18012a9c060d7d994d4cb
Prashant Ghimire <111095724+Prsnt95@users.noreply.github.com>
2024-06-10 17:32:40 +0300

    1st commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,29 @@
-import React, { Component } from 'react';
-  
+import React, { Component } from "react";
+import "./About.css";
+import pfp from "../assets/pfp.png";
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div className="split left">
+          <div className="centered">
+            <img
+              className="profile_image"
+              src={pfp} // Image goes here
+              alt="Profile Pic"
+            ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Your Name</div>
+            <div className="brief_description">
+              Hi my name is Prashant, I am a computer science major and rising
+              junior at Northwestern Univeristy. I like to play soccer.
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
